bb1ee58cf51143aa100d21543b270be8b094c939
Aaryan Agrawal <aaryanagrawal03@gmail.com>
2021-05-03 22:36:02 -0700

    clean up clean up

--- new_scrape.py
+++ new_scrape.py
@@ -3,48 +3,70 @@ from psaw import PushshiftAPI
 import pandas as pd
 import datetime as dt
 
-with open("pw.txt", "r") as f:
-    pw = f.read()
+with open("credentials.txt", "r") as f:
+    creds = [c.strip() for c in f]
     reddit = praw.Reddit(
         client_id="WcHieQApODE5EA",
-        client_secret="saiU9E_3sWIazKUU4f4ngaACoSs7Qw",
-        password=pw,
+        client_secret=creds[0],
+        password=creds[1],
         user_agent="MechGraph/0.0.0",
         username="ManyManyDucks",
     )
+
 api = PushshiftAPI(reddit)
 
-start_epoch = int(dt.datetime(2021, 4, 1).timestamp())
-end_epoch = int(dt.datetime(2021, 5, 1).timestamp())
+limit_2021 = 4
 
-df = pd.DataFrame()
+times = [int(dt.datetime(2020, m, 1).timestamp()) for m in range(1, 13)] + [
+    int(dt.datetime(2021, m, 1).timestamp()) for m in range(1, limit_2021 + 2)
+]
 
-gen = api.search_submissions(
-    after=start_epoch,
-    before=end_epoch,
-    subreddit="mechmarket",
-    q="gmk",
-)
+months = [
+    "january",
+    "february",
+    "march",
+    "april",
+    "may",
+    "june",
+    "july",
+    "august",
+    "september",
+    "october",
+    "november",
+    "december",
+]
 
-count = 0
-for c in gen:
-    if (
-        c.link_flair_text in ["Selling", "Sold", "Trading"]
-        and c.removed_by_category is None
-    ):
-        df = df.append(
-            {
-                "link": c.permalink,
-                "date": c.created_utc,
-                "title": c.title,
-                "post": c.selftext,
-            },
-            ignore_index=True,
-        )
+names = [m + "2020" for m in months] + [m + "2021" for m in months[:limit_2021]]
 
-    count += 1
 
-    if count % 1000 == 0:
-        print(count)
+def get_data(start, end, name):
+    print(f"Starting data collection for: {name}")
+    df = pd.DataFrame()
 
-df.to_csv("testing.csv")
+    gen = api.search_submissions(
+        after=start,
+        before=end,
+        subreddit="mechmarket",
+        q="gmk",
+    )
+
+    for c in gen:
+        if (
+            c.link_flair_text in ["Selling", "Sold", "Trading"]
+            and c.removed_by_category is None
+        ):
+            df = df.append(
+                {
+                    "link": c.permalink,
+                    "date": c.created_utc,
+                    "title": c.title,
+                    "post": c.selftext,
+                },
+                ignore_index=True,
+            )
+
+    df.to_csv(f"datasets/{name}.csv")
+
+
+for i, name in enumerate(names):
+    get_data(times[i], times[i + 1], name)
