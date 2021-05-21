bf90d9e95d22db1fd3d8adeb308cc4a883816839
Aaryan Agrawal <aaryanagrawal03@gmail.com>
2021-05-20 23:35:41 -0700

    intro graphing

--- mm-tracker/src/App.js
+++ mm-tracker/src/App.js
@@ -3,13 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 import firebase from './firebase.js';
 
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from "recharts";
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
       setname: '',
       categoryname: '',
-      items: []
+      items: [],
+      averageMonths: []
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -24,6 +35,8 @@ class App extends Component {
   handleSubmit(e) {
     e.preventDefault();
     let newItems = [];
+    let sortingMonths = {};
+    let averageMonths = [];
     const db = firebase.firestore();
     db.collection("gmk").doc(this.state.setname).collection('sales')
       .orderBy("date").get().then(querySnapshot => {
@@ -31,14 +44,36 @@ class App extends Component {
           console.log(doc.id, " => ", doc.data());
           let newData = doc.data();
           newData.id = doc.id;
-          newData.date = newData.date.toDate().toString();
+          let dateConvert = newData.date.toDate();
+          let key = [dateConvert.getFullYear(), dateConvert.getMonth()]
+          newData.date = dateConvert.toString();
+          if (newData.category == this.state.categoryname) {
+            if (sortingMonths[key]) {
+              sortingMonths[key].push(newData.price);
+            } else {
+              sortingMonths[key] = [newData.price];
+            }
+          }
           newItems.push(newData);
-          console.log(newItems);
-        });
 
+
+        });
+        console.log(sortingMonths);
+        for (let key in sortingMonths) {
+          if (sortingMonths.hasOwnProperty(key)) {
+            const average = sortingMonths[key].reduce((acc, c) => acc + c, 0) / sortingMonths[key].length;
+            averageMonths.push({
+              month: key,
+              average: average,
+              quantity: sortingMonths[key].length,
+            });
+          }
+
+        }
         this.setState({
           currentItem: '',
-          items: newItems
+          items: newItems,
+          averageMonths: averageMonths
         });
         console.log(this.state)
       });
@@ -46,6 +81,15 @@ class App extends Component {
 
 
   render() {
+    let data = [];
+    for (let i in this.state.averageMonths) {
+      let ob = this.state.averageMonths[i]
+      data.push({
+        name: ob.month,
+        avg: ob.average,
+      })
+    }
+
     return (
       <div className='app'>
         <header>
@@ -53,7 +97,20 @@ class App extends Component {
             <h1>Loser's Club</h1>
           </div>
         </header>
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="linear" dataKey="avg" stroke="#82ca9d" />
+        </LineChart>
         <div className='container'>
+
           <section className='add-item'>
             <form onSubmit={this.handleSubmit}>
               <input type="text" name="setname" placeholder="What set" onChange={this.handleChange} value={this.state.setname} />
@@ -64,6 +121,15 @@ class App extends Component {
           <section className='display-item'>
             <div className="wrapper">
               <ul>
+
+                {this.state.averageMonths.map((item) => {
+                  return (
+                    <li key={item.month}>
+                      <h3>{item.month}</h3>
+                      <p>average: {item.average}</p>
+                    </li>
+                  )
+                })}
                 {this.state.items.map((item) => {
                   return (
                     <li key={item.id}>
